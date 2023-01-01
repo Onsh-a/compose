d05d64dbbddd4f4a848d2018ed9da26537686ce2
Arseniy Zakharov <a.zaharov@aristos.pw>
2023-01-02 02:02:24 +0300

    Tuner: some upgrades for tuner functionality

--- src/js/data/tunings.ts
+++ src/js/data/tunings.ts
@@ -6,6 +6,14 @@ const tunings: { [index: string]: { [index: string]: number} } = {
     D4: 146.83,
     A5: 110.00,
     E6: 82.41,
+  },
+  drop_d: {
+    E1: 329.63,
+    B2: 246.94,
+    G3: 196.00,
+    D4: 146.83,
+    A5: 110.00,
+    D6: 73.42,
   }
 }
 export default tunings;
